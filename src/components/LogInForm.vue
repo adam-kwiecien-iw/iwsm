8c02392b46b57624d1d3eeaa7bb5ba17fc49e53e
seulgi <seulgi.lee@integration.works>
2022-01-21 13:02:11 +1300

    Add a font

--- src/components/LogInForm.vue
+++ src/components/LogInForm.vue
@@ -82,12 +82,12 @@ export default {
 
 
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap');
-
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
  * {
      margin: 0;
      padding: 0;
      box-sizing: border-box;
-     font-family: 'Poppins', sans-serif
+     font-family: 'Open Sans', sans-serif
  }
 
  body {
